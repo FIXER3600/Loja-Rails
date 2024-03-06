be17facf4162d898cfeb5ed50deb7d4aeef1a743
FIXER3600 <guilhermecomercial51@gmail.com>
2024-03-06 15:10:45 -0300

    Adicionando espaçamento nas telas e link de remoção

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -6,5 +6,6 @@ class ProdutosController < ApplicationController
   def create
     produto= params.require(:produto).permit(:nome,:descricao,:preco,:quantidade)
     Produto.create produto
+    redirect_to root_path
   end
 end
