9372dd38fc61380e0658f83da8bc7b4c563e7708
FIXER3600 <guilhermecomercial51@gmail.com>
2024-03-13 17:52:40 -0300

    Refatorando views

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -10,7 +10,7 @@ class ProdutosController < ApplicationController
       flash[:notice]="Produto criado com sucesso!"
       redirect_to root_url
     else
-      renderiza
+      renderiza :new
       end
   end
 
@@ -24,7 +24,7 @@ class ProdutosController < ApplicationController
     redirect_to root_path
   end
   def edit
-    renderiza
+    renderiza :edit
   end
   def update
 
@@ -33,21 +33,23 @@ class ProdutosController < ApplicationController
       flash[:notice]="Produto atualizado com sucesso!"
       redirect_to root_url
     else
-      renderiza
+      renderiza :edit
     end
   end
   def busca
     @nome=params[:nome]
     @produtos= Produto.where "nome like ?","%#{@nome}%"
   end
+
+  private
   def produto_params
     params.require(:produto).permit(:nome,:descricao,:preco,:quantidade,:departamento_id)
   end
   def set_produto
     @produto=Produto.find(params[:id])
   end
-  def renderiza
+  def renderiza(view)
     @departamentos=Departamento.all
-    render :new
+    render view
   end
 end
