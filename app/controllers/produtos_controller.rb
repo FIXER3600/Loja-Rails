de388e1d02d241a0088bfb727c4aa8edf2115b66
FIXER3600 <guilhermecomercial51@gmail.com>
2024-03-13 17:24:55 -0300

    Refatorando controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,16 +1,16 @@
 class ProdutosController < ApplicationController
+  before_action :set_produto,only: [:edit,:update,:destroy]
   def index
     @produtos=Produto.order(nome: :desc).limit 2
     @produto_com_desconto=Produto.order(:preco).limit 1
   end
   def create
-    values= params.require(:produto).permit(:nome,:descricao,:preco,:quantidade,:departamento_id)
-    @produto=Produto.new values
+    @produto=Produto.new produto_params
     if @produto.save
       flash[:notice]="Produto criado com sucesso!"
       redirect_to root_url
     else
-      render :new
+      renderiza
       end
   end
 
@@ -19,32 +19,35 @@ class ProdutosController < ApplicationController
     @departamentos=Departamento.all
   end
   def destroy
-    id= params[:id]
-    Produto.destroy(id)
+    @produto.destroy
     flash[:notice]="Produto deletado com sucesso!"
     redirect_to root_path
   end
   def edit
-    id= params[:id]
-    @produto=Produto.find(id)
-    @departamentos=Departamento.all
-    render :new
+    renderiza
   end
   def update
-    id= params[:id]
-    @produto=Produto.find(id)
-    values= params.require(:produto).permit(:nome,:descricao,:preco,:quantidade,:departamento_id)
-    @produto=Produto.new values
-    if @produto.update values
+
+    @produto=Produto.new produto_params
+    if @produto.update produto_params
       flash[:notice]="Produto atualizado com sucesso!"
       redirect_to root_url
     else
-      @departamentos=Departamento.all
-      render :new
+      renderiza
     end
   end
   def busca
     @nome=params[:nome]
     @produtos= Produto.where "nome like ?","%#{@nome}%"
+  end
+  def produto_params
+    params.require(:produto).permit(:nome,:descricao,:preco,:quantidade,:departamento_id)
+  end
+  def set_produto
+    @produto=Produto.find(params[:id])
+  end
+  def renderiza
+    @departamentos=Departamento.all
+    render :new
   end
 end
